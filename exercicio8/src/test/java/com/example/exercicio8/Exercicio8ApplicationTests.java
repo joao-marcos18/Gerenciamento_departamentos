5eb1616645e3badb569cf62cac4d9a98e0c5d5ed
joao-marcos18 <joao.moreiranunes10@gmail.com>
2026-03-18 13:26:23 -0300

    Projeto criado

---
+++ exercicio8/src/test/java/com/example/exercicio8/Exercicio8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicio8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
